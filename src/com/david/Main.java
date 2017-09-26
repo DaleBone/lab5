f5eade8ec3ae6ad6585a6139a3ccc1bfab252511
David <waterisbetter@mail.com>
2017-09-26 11:20:25 -0500

    added try/catch

--- src/com/david/Main.java
+++ src/com/david/Main.java
@@ -10,43 +10,57 @@ import java.io.FileReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         String name = "David";
         String filename = "name.txt";
 
-        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(filename));
+        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(filename))){
 
-        fileWriter.write(name + "\n");
-        fileWriter.write("Java Programming");
-        fileWriter.newLine();
+            fileWriter.write(name + "\n");
+            fileWriter.write("Java Programming");
+            fileWriter.newLine();
 
-        fileWriter.write(2545 + "\n");
-        fileWriter.newLine();
+            fileWriter.write(2545 + "\n");
+            fileWriter.newLine();
 
-        fileWriter.write("Tomorrow is Wednesday." + "\n");
-        fileWriter.write("Yo Yo YO YO!");
-        fileWriter.write(2620);
-        fileWriter.newLine();
+            fileWriter.write("Tomorrow is Wednesday." + "\n");
+            fileWriter.write("Yo Yo YO YO!");
+            fileWriter.write(2620);
+            fileWriter.newLine();
 
-        fileWriter.write(Double.toString(123.45));
+            fileWriter.write(Double.toString(123.45));
 
-        fileWriter.close();
+            fileWriter.close();
 
-        System.out.println("Your name has been written to " + filename);
+            System.out.println("Your name has been written to " + filename);
+
+        } catch (IOException ioe) {
+
+            System.out.println("Error writing to file.");
+            System.out.println(ioe);
+        }
 
         //FileReader reader = new FileReader("name.txt");
         //BufferedReader bufReader = new BufferedReader(reader);
-        BufferedReader fileReader = new BufferedReader(new FileReader(filename));
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(filename))){
 
-        String line = fileReader.readLine();
-        
-        while (line != null){
-            System.out.println(line);
+            String line = fileReader.readLine();
 
-            line = fileReader.readLine();
+            while (line != null) {
+                System.out.println(line);
+
+                line = fileReader.readLine();
+            }
+
+            fileReader.close();
+        } catch(IOException ioe){
+
+            System.out.println("Could not open ore read " + filename);
+            System.out.println(ioe.toString());
         }
 
-        fileReader.close();
+        System.out.println("If the code did not work, the program did not crash");
+        System.out.println("The program keeps running so could perhaps ask the user " + "to create this file, or specify the file location");
     }
 }
